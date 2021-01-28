92ecf76b78e9193741723bac0e1d2f9f91e7b12d
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-28 19:46:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_item =
   min_val= 300
 
-  name_hash.each do |key, value| unless empty?
+  name_hash.each do |key, value|
     if value < min_val
       lowest_item = key
       min_val = valuexs
