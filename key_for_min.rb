932347fef3e13ecadf46a2d68837d21d8f4e9964
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-28 19:36:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,16 @@
 # require 'pry'
 
 def key_for_min_value(name_hash)
-  # lowest_item = ""
+  lowest_item = ""
   min_val= 300
 
   name_hash.each do |key, value|
     if value < min_val
-      # lowest_item = key
+      lowest_item = key
       min_val = value
       #  binding.pry
     end
 
   end
-  # lowest_item
-  key
+  lowest_item
 end
